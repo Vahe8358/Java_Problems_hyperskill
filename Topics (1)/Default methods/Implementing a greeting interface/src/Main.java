59e3b000c4a0bda3009c8185402a54e38f4f2d1d
Vahe8358 <vahovhannisyan@griddynamics.com>
2025-06-24 16:17:47 +0400

    new tasks

---
+++ Topics (1)/Default methods/Implementing a greeting interface/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+interface Greetings {
+    // TODO: Define the default 'greet' method here
+    default String greet(String name){
+        return "Hello, " + name + "!";
+    }
+}
+
+class GreetingsImpl implements Greetings {
+    // TODO: Implement the 'greet' method here
+
+    @Override
+    public String greet(String name) {
+        return Greetings.super.greet(name);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        Greetings greetings = new GreetingsImpl();
+        String greeting = greetings.greet(name);
+        System.out.println(greeting);
+    }
+}
